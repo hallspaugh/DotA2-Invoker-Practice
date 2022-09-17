4c1f75571095091e387e55b081918a1cadf07f8c
hallspaugh <hunter.allspaugh@sproutsocial.com>
2022-09-17 11:15:44 -0500

    Added Line

--- webpack-config.js
+++ webpack-config.js
@@ -1,31 +1,21 @@
-const webpack = require('webpack');
-const path = require('path');
-
 module.exports = {
-    entry: [
-        './src/app.js'
-    ],
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'app.bundle.js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.html$/,
-                loader: 'file-loader?name=[name].[ext]',
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        },
-    ],
-},
-plugins: [
-    new webpack.NamedModulesPlugin(),
-]
-};
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  mode: 'development'
+};;
